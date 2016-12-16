956066019a2bef16d465fd28ed569049aec510c1
Baptiste Charrier <baptistech@hotmail.fr>
2016-12-16 14:04:44 +0100

    Node class and child request

--- DriveOp.py
+++ DriveOp.py
@@ -16,10 +16,18 @@ def main():
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('drive', 'v3', http=http)
 
-    results = service.files().list(q="'me' in owners and not mimeType contains 'application/vnd.google-apps'",
+    results = service.files().list(q="'me' in owners and  mimeType contains 'application/vnd.google-apps.folder'",
         pageSize=1000,
         spaces="drive",
         fields="files(id, name, size, mimeType, quotaBytesUsed)").execute()
+
+#0BwjaEAUUi370ZVBKNXFheFRCWUE
+    child = service.files().list(q="'0BwjaEAUUi370QWdEeWY2eTR6YjQ' in parents and 'me' in owners",
+                                 fields="files(id, name, size, mimeType, quotaBytesUsed)", pageSize=1000).execute()
+    childrens = child.get('files', [])
+    for itm in childrens:
+        print(itm)
+
     items = results.get('files', [])
     if not items:
         print('No files found.')
@@ -27,11 +35,5 @@ def main():
         print('Files:')
         print(len(items))
         s = 0
-        for item in items:
-            s += DriveManip.getMoUsed(item["quotaBytesUsed"])#(int(item["quotaBytesUsed"])/1024)/1024
-            if ((int(item["quotaBytesUsed"])/1024)/1024 > 100):
-                print(item)
-
-        print(s)
 
 main()
