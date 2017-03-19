33942c92474c657c010174f4b71c1757be266175
Kyochi <baptistech@hotmail.fr>
2017-03-19 15:12:09 +0100

    dfs partially working

--- DriveOp.py
+++ DriveOp.py
@@ -2,8 +2,8 @@ import Node
 
 class DriveOp:
     @staticmethod
-    def getMo(self, bytesUsed):
-        return int(bytesUsed)/1024/1024
+    def getMo(bytesUsed):
+        return (bytesUsed)/1024/1024
 
     @staticmethod
     def jsonBinarySearch(jsonList, attributeToCompare, elementTarget):
@@ -30,33 +30,38 @@ class DriveOp:
         while (jsonList[lower][target][0] ==  targetValue and lower >= 0):
             lower-=1
 
-        while (jsonList[higher][target][0] ==  targetValue and higher < len(jsonList)):
+        while (jsonList[higher][target][0] ==  targetValue and higher < len(jsonList)-1):
             higher+=1
 
         bounds.append(lower + 1)
-        bounds.append(higher - 1)
+        bounds.append(higher)
 
         return bounds
 
     @staticmethod
     def dfsDriveSize(jsonList, curNode):
+
         posChild = DriveOp.jsonBinarySearch(jsonList, "parents", curNode.m_id)
         if (posChild == -1):
             if (curNode.m_type != 'gdoc'):
-                index = DriveOp.jsonBinarySearch(jsonList, "id", curNode.m_id)
-                return DriveOp.getMo((jsonList[index])['size'])
+                curNode.m_size = float((jsonList[curNode.m_index])['size'])
+                return curNode.m_size
             else:
                 return 0
 
+        curNode.m_childs = []
         rangeParents = DriveOp.getIdRange(jsonList, "parents", posChild)
+        indexCurrentElement = rangeParents[0]
         for element in jsonList[rangeParents[0]: rangeParents[1]]:
             if (element['mimeType'].startswith("application/vnd.google-apps")):
-                child = Node.Node("gdoc", 0, element['id'], element['name'])
+                child = Node.Node("gdoc", 0, element['id'], element['name'], indexCurrentElement)
             else :
-                child = Node.Node(element['mimeType'], 0, element['id'], element['name'])
+                child = Node.Node(element['mimeType'], 0, element['id'], element['name'], indexCurrentElement)
 
             curNode.m_size = curNode.m_size + DriveOp.dfsDriveSize(jsonList, child)
             (curNode.m_childs).append(child)
+            indexCurrentElement = indexCurrentElement + 1
 
+        return curNode.m_size
 
 
--- Main.py
+++ Main.py
@@ -39,10 +39,23 @@ def main():
         print('Total Files:')
         print(len(rootfs))
 
-    nodeRootFolder = Node.Node("Folder", 0, rootid, "root")
+    nodeRootFolder = Node.Node("application/vnd.google-apps.folder", 0, rootid, "rootDrive", -1)
 
+    i = 0
+    n = 0
     for itemSorted in rootFolderSorted:
         print(itemSorted)
+        if (not(itemSorted['mimeType'].startswith("application/vnd.google-apps"))):
+            i = i + float(itemSorted['size'])
+            n = n+1
+
+    print(i)
+    print(n)
 
     op = DriveOp()
+    size = op.dfsDriveSize(rootFolderSorted, nodeRootFolder)
+    print(len(rootFolderSorted))
+    print(size)
+    print(op.getMo(size))
+
 main()
